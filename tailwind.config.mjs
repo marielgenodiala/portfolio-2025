87f8a24f45173d32827539517ba53c8937eefd1a
marielgenodiala <genodialamariel1701@gmail.com>
2025-10-26 22:40:02 +0800

    chore: Update configuration and dependencies for Tailwind CSS integration and styling improvements

---
+++ tailwind.config.mjs
@@ -0,0 +1,29 @@
+/** @type {import('tailwindcss').Config} */
+
+import defaultTheme from "tailwindcss/defaultTheme";
+export default {
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        "dim-gray": "#7B6D62",
+        "van-dyke": "#423A37",
+        "rich-black": "#0E191F",
+        gunmetal: "#2B3C43",
+        "paynes-gray": "#597276",
+      },
+      fontFamily: {
+        heading: ["Inter", "sans-serif"],
+        button: ["Montserrat", "sans-serif"],
+        text: ["var(--font-poppins)", ...defaultTheme.fontFamily.sans],
+        poppins: ["var(--font-poppins)", ...defaultTheme.fontFamily.sans],
+        body: ["var(--font-poppins)", ...defaultTheme.fontFamily.sans],
+      },
+    },
+  },
+  plugins: [],
+};
